7ef9daa48f1c3e4615a754d68183edbc55301035
niko-pfeffi <nikolas.joop@pfeffermind.de>
2026-05-07 15:25:40 +0200

    fix paths

--- addons/scorm/plugin.gd
+++ addons/scorm/plugin.gd
@@ -3,13 +3,13 @@ extends EditorPlugin
 
 const AUTOLOAD_NAME = "Scorm"
 
-const EXPORT_PLUGIN_SCRIPT := preload("res://addons/scorm/scorm_export.gd")
+const EXPORT_PLUGIN_SCRIPT := preload("res://addons/scorm-godot-plugin/addons/scorm/scorm_export.gd")
 
 var _export_plugin = null
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/scorm/scorm.gd")
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/scorm-godot-plugin/addons/scorm/scorm.gd")
 	_export_plugin = EXPORT_PLUGIN_SCRIPT.new()
 	add_export_plugin(_export_plugin)
 
